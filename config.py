e5c99b71273b48142427e9dd40aef16729d0f087
Ming Zhu <zhumingpassional@gmail.com>
2022-05-29 11:30:22 +0800

    add multi nodes

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 
-NUMS_NODES = [4, 6, 8, 10]
+NUMS_NODES = [4, 6, 8, 10]  # a list indicates the num of nodes.
+
 ADJACENCY_MATRIX_DIR = "adjacency_matrix"
 
 ADJACENCY_MATRIX = []
 
-RESULT_DIR = "result"
+RESULT_DIR = "result"  # dir of result using gurobi
 
 
 
