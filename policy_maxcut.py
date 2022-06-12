e6a9dd20c013fd42f0c40a0d87b096c7f5eb2d0f
shixun404 <shixun404@gmail.com>
2022-06-11 23:41:31 -0400

    env

--- policy_maxcut.py
+++ policy_maxcut.py
@@ -3,11 +3,11 @@ import numpy as np
 class Maxcut:
     def __init__(self, adjacency_matrix):
         self.adjacency_matrix = adjacency_matrix
-        self.n = adjacency_matrix.shape[0]
-        self.state = np.random.rand(self.n)
+        self.N = adjacency_matrix.shape[0]
+        self.state = np.random.rand(self.N)
     
     def reset(self, ):
-        self.state = np.random.rand(self.n )
+        self.state = np.random.rand(self.N )
         return self.state
     
     def step(self, action):
@@ -15,5 +15,10 @@ class Maxcut:
         reward = self.get_reward(self.clip)
         done = False
         return self.state, reward, done, {}
-    def get_reward(self,):
-        for i in range()
+
+    def calc_H(self, configure):
+        H = 0
+        for i in range(self.N):
+            for j in range(self.N):
+                H -= configure[i] * (1 - configure[j]) * self.adjacency[i,j]
+        return H
