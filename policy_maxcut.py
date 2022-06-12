dfb99cbc0c717e671d8b7d60a0558dadcdd38a1c
shixun404 <shixun404@gmail.com>
2022-06-11 23:30:07 -0400

    policy gradient env

--- policy_maxcut.py
+++ policy_maxcut.py
@@ -1,13 +1,15 @@
 import torch as th
-
+import numpy as np
 class Maxcut:
-    def __init__():
+    def __init__(self, adjacency_matrix):
+        self.adjacency_matrix = adjacency_matrix
+        self.n = adjacency_matrix.shape[0]
+        self.state = np.random.rand(self.n)
+    
+    def reset(self, ):
+        self.state = np.random.rand(() )
+    
+    def step(self, ):
         None
-    
-    def reset():
-    
-    def step():
-    
-    def get_reward():
-        
-    def 
+    def get_reward(self, tate):
+        for i in range()
