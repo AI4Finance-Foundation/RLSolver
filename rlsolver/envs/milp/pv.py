ae7155bd50346e850b2c83349bce190c32a67d1d
Xiao-Yang Liu <xl2427@columbia.edu>
2022-08-30 09:02:27 -0400

    Update pv.py

--- rlsolver/envs/milp/pv.py
+++ rlsolver/envs/milp/pv.py
@@ -1,1 +1,3 @@
-# public vehicle
+# Public vehicles for future urban transportation. 
+# IEEE transactions on intelligent transportation systems, 17(12), 3344-3353, 2016.
+# Zhu, M., Liu, X. Y., Tang, F., Qiu, M., Shen, R., Shu, W., & Wu, M. Y.
