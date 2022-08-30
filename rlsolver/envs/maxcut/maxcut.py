cd66dff1dd9af99852500e2f9b6e6d2286b6639d
shixun404 <shixun404@gmail.com>
2022-08-30 02:21:52 -0700

    mimo

--- rlsolver/envs/maxcut/maxcut.py
+++ rlsolver/envs/maxcut/maxcut.py
@@ -7,16 +7,14 @@ from tqdm import tqdm
 import sys
 
 
-def build_mlp(mid_dim: int, num_layer: int, input_dim: int, output_dim: int):  # MLP (MultiLayer Perceptron)
+def build_mlp(mid_dim: int, num_layer: int, input_dim: int, output_dim: int): 
     assert num_layer >= 1
     net_list = list()
     if num_layer == 1:
         net_list.extend([nn.Linear(input_dim, output_dim), ])
-    else:  # elif num_layer >= 2:
+    else:
         net_list.extend([nn.Linear(input_dim, int(input_dim / 4) ), nn.ReLU()])
-        #for _ in range(num_layer - 2):
         net_list.extend([nn.Linear(int(input_dim / 4), int(input_dim / 10) ), nn.ReLU()])
-        #net_list.extend([nn.Linear(int(input_dim / 4), int(input_dim / 8) ), nn.ReLU()])
         net_list.extend([nn.Linear(int(input_dim / 10), int(output_dim * 5)), nn.ReLU()])
         net_list.extend([nn.Linear(int(output_dim * 5), output_dim), ])
     return nn.Sequential(*net_list)
@@ -80,8 +78,6 @@ class DHN:
             
             for i in range(self.T):
                 non_zero_edge = np.where(self.adjacency_mat[v] == 1)[0]
-                #print(non_zero_edge)
-                #print(np.random.choice(non_zero_edge))
                 v = np.random.choice(non_zero_edge)
                 traj.append(v)
             
@@ -108,45 +104,17 @@ class DHN:
                     #obj += p[0,i,0] * p[0,j,1] * self.adjacency_mat[i][j] * 10
                     print(p) 
                     obj_1 += p[0, i] * (1 - p[0, j]) * self.adjacency_mat[i][j] * 10000 #* self.dist[i] * self.dist[j]
-            
-            #obj_1 = 0 
             print("befor explore loss is ", -obj_1.item())
-            
-            '''
-            l = min(self.N, len(self.replay_buffer))
-            l_ = np.random.randint(0, len(self.replay_buffer), [l])
-            
-            obj_2 = 0
-            for j in l_:
-                traj = self.replay_buffer[j]
-                g = 1
-                for i in range(1,len(traj)):
-                    #assert 0
-                    obj_2 += p[0,traj[i - 1]] * (1 - p[0,traj[i]]) * g * self.adjacency_mat[traj[i - 1]][traj[i]] * 10000 * self.dist[traj[i - 1]] * self.dist[traj[i]]
-                    obj_2 += (1 - p[0,traj[i - 1]]) * p[0,traj[i]] * g* self.adjacency_mat[traj[i - 1]][traj[i]] * 10000 * self.dist[traj[i - 1]] * self.dist[traj[i]]
-                    g *= self.gamma
-            #obj_1 = obj_1
-            #obj += obj_1
-            obj_1 += obj_2 * 0.1
-            '''
             print("After explore dist loss is ",-obj_1.item())
             print("unif loss is ", -obj.item())
             obj_1 = -(obj_1) 
-            #obj.retain_grad()
-            #print("loss is ", obj_1.item())
             self.obj_record.append(obj.item())
             self.optimizer.zero_grad()
-            #print(obj.grad)
-            #obj.retain_grad()
             obj_1.backward()
-            #obj.retain_grad()
-            #print(obj.grad)
-            #clip_grad_norm_(parameters=self.optimizer.param_groups[0]["params"], max_norm=self.clip_grad_norm)
             self.optimizer.step()
     
     def save(self,):
         import os
-        #print(self.obj_record)
         exp_id = 0
         if self.exp_id == -1:
             
@@ -162,16 +130,7 @@ class DHN:
             self.exp_id = exp_id
             os.mkdir('./{}/{}/'.format(self.action_dim, exp_id))
         exp_id = self.exp_id
-        #print("Objective function is:\n")
-        #print([self.obj_record[i] for i in range(0, len(self.obj_record), 100)])
-        #import plotext as plt
-        #plt.plot(self.obj_record)
-        #plt.title("Exp {} Obj ".format(exp_id))
-        #plt.show()
         print("Finished experiment {}.".format(exp_id))
-
-
-        #os.mkdir('./{}/{}/'.format(self.action_dim, exp_id))
         t = self.action_dim
         path = './{}/{}/policy.pth'.format(self.action_dim, exp_id)
         matrix_file = ''
@@ -184,7 +143,7 @@ class DHN:
         
 
     def load(self, n, exp_id):
-        #self.policy.load_state_dict(torch.load('./' + str(n) + '/' + str(exp_id) + '/policy.pth', map_location=torch.device(self.device)))
+        self.policy.load_state_dict(torch.load('./' + str(n) + '/' + str(exp_id) + '/policy.pth', map_location=torch.device(self.device)))
         self.adjacency_mat = np.load('./' + str(n) + '/' + str(exp_id) + '/adjacency.npy')
         
     def test(self,):
@@ -254,9 +213,6 @@ def run(seed=1, gpu_id = 0, v_num = 10):
     np.random.seed(seed + int(time.time()))
     s = v_num
     mat,dist = gen_adjacency_matrix_unweighted(s, 0.05)
-    #mat =[ [0, 1, 1],[1, 0, 0], [1, 0, 0]]
-    #mat = star(v_num)
-    #print(mat)
     agent = DHN(adjacency_mat = mat, state_dim=s, action_dim=s, gpu_id=gpu_id)
     agent.load(100, 20)
     agent.dist = cal_dist(agent.adjacency_mat)
@@ -277,6 +233,5 @@ def run(seed=1, gpu_id = 0, v_num = 10):
 if __name__ =='__main__':
     GPU_ID = int(sys.argv[1]) if len(sys.argv) > 1 else 0  # >=0 means GPU ID, -1 means CPU
     v_num = int(sys.argv[2]) if len(sys.argv) > 2 else 10
-    #ENV_ID = int(sys.argv[3]) if len(sys.argv) > 3 else 1  
     while True:
         run(1, GPU_ID, v_num)
--- rlsolver/envs/mimo_beamforming/mimo_beamforming.py
+++ rlsolver/envs/mimo_beamforming/mimo_beamforming.py
@@ -1,111 +1,71 @@
+from net import Policy_Net_MIMO
 import torch as th
-import numpy as np
+import time
 import os
-class beamforming_env:
-    def __init__(self, num_antennas = 2, num_users = 2, total_power = 10, noise_power = 1, path_loss_option=False, path_loss_range=[-5, 5], H_transition=False):
-        self.state = None
-        self.N = num_antennas
-        self.K =  num_users
-        self.total_power = 10
-        self.noise_power = 1
-        self.path_loss_option = path_loss_option
-        self.path_loss_range = path_loss_range
-        self.max_step = 50
-        self.encode_dim = 32
-        self.user_weights = np.ones(self.K)
-        self.wsr = 0
-        self.selected_users = [i for i in range(self.K)]
-        self.H_test = th.zeros((10,2,2), dtype=th.cfloat)
-    
-        if os.path.exists("./test_H_K2N2.npy"):
-            print("True")
-            with open("./test_H_K2N2.npy", "rb") as f:
-                tmp = np.load(f)
-            self.H_test = th.as_tensor(tmp)
 
+
+def train_mimo( policy_net_mimo, optimizer, curriculum_base_vectors, num_users=4, num_antennas=4, total_power=10, noise_power=1, num_training_epochs=40000,
+                num_subspace_update_gap=400, num_save_model_gap=1000, episode_length=5, fullspace_dim=32, cur_subspace=1, batch_size=4096, 
+                device=th.device("cuda:0" if th.cuda.is_available() else "cpu")):
+    
+    for epoch in range(num_training_epochs):
+        if (epoch + 1) % num_subspace_update_gap == 0 and cur_subspace < 32:
+            cur_subspace +=1
+            vec_H = generate_batch_channel(num_antennas, num_users, fullspace_dim, batch_size, cur_subspace, curriculum_base_vectors).to(device)
         else:
-            with open("./test_H_K2N2.npy", "wb") as f:
-                np.save(f, self.H_test.numpy())
-    
-    
-        self.H_mu = 0
-        self.H_sigma = 1   
-        self.device=th.device("cuda:0")
-        self.step_ = 0
-    
-    def reset(self, H = -1):
-        if H == -1:
-            _, _, H, _ = self.compute_channel(self.N, self.K, self.total_power,np.array([self.H_[:, :, 0]]), self.path_loss_option, self.path_loss_range, std = 1.0)
-            H = th.diag(th.randn(2, dtype=th.cfloat))
-        else:
-            H = self.H_test[H]
-        self.H = th.as_tensor(H, dtype=th.cfloat).to(self.device)
-        self.W = th.randn(self.K, self.N, dtype=th.cfloat).to(self.device)
-        self.step_ = 0
-        self.wsr = self.calc_wsr(self.user_weights, self.H.unsqueeze(0), self.W.unsqueeze(0), self.noise_power, self.selected_users)
-    
-        return self.get_state()
-    
-    def get_state(self,):
+            vec_H = th.randn(batch_size, fullspace_dim, dtype=th.cfloat).to(device)
+            
+        mat_H = (vec_H[:, :num_users * num_antennas] + vec_H[:, num_users * num_antennas:] * 1.j).reshape(-1, num_users, num_antennas)
+        mat_W = policy_net_mimo.calc_mmse(mat_H).to(device)
+        vec_W= th.cat((mat_W.real.reshape(-1, num_users * num_antennas), mat_W.imag.reshape(-1, num_users * num_antennas)), 1)
+        loss = 0
+        for _ in range(episode_length):
+            mat_HW = th.bmm(mat_H, mat_W.transpose(1,2).conj())
+            vec_HW = th.cat((mat_HW.real.reshape(-1, num_users * num_antennas), mat_HW.imag.reshape(-1, num_users * num_antennas)), 1)
+            vec_W = policy_net_mimo(th.cat((vec_H, vec_W, vec_HW), 1), mat_HW, mat_H)
+            mat_W = (vec_W[:, :num_users * num_antennas] + vec_W[:, num_users * num_antennas:] * 1.j).reshape(-1, num_users, num_antennas)
+            loss -= policy_net_mimo.calc_sum_rate(mat_H, mat_W).sum()
         
-        mmse_net_input = self.H  
-        mmse_net_target = self.W.to(th.cfloat)
-        tmp = mmse_net_input.to(th.cfloat)
-        mmse_net_input= th.cat((th.as_tensor(mmse_net_input.real).reshape(self.K * self.N), th.as_tensor(mmse_net_input.imag).reshape(self.K * self.N)), 0)
-        h_w_input = th.matmul(tmp, mmse_net_target.transpose(0,1).conj())
-        h = h_w_input
-        mmse_net_target= th.cat((th.as_tensor(mmse_net_target.real).reshape(self.K * self.N), th.as_tensor(mmse_net_target.imag).reshape(self.K * self.N)), 0)
-        h_w_input= th.cat((th.as_tensor(h_w_input.real).reshape(self.K * self.N), th.as_tensor(h_w_input.imag).reshape(self.K * self.N)), 0)
-        
-        self.state = th.cat((mmse_net_input.reshape(self.N * 2*self.K), 
-                            mmse_net_target.reshape(self.N * 2* self.K), 
-                            h_w_input.reshape(self.N * 2* self.K), tmp.reshape(self.N* self.K), 
-                            h.reshape(self.K* self.K)), dim=0)
-        return self.state  
-  
-    def step(self, action):
-    
-        precoder = action.reshape(2, self.K, self.N)
-        precoder = precoder[0] + precoder[1] * 1.j
-        precoder = precoder.reshape(self.K,self.N)
-        self.W = precoder #+ self.W
-        if self.H_transition:
-            self.H = self.H + th.normal(self.H_mu, self.H_sigma, (self.K, self.N))
-        wsr = self.calc_wsr(self.user_weights, self.H.unsqueeze(0), self.W.unsqueeze(0), self.noise_power, self.selected_users)
-        reward = wsr - self.wsr
-        self.wsr = wsr
-        self.step_ += 1
-        done = False
-        if self.step_ > self.max_step:
-            done = True
-        return self.get_state(), reward, done, {}
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        print(f" training_loss: {loss.mean().item():.3f} | gpu memory: {th.cuda.memory_allocated():3d}")
+        if epoch % num_save_model_gap == 0:
+            th.save(policy_net_mimo.state_dict(), save_path+f"{epoch}.pth")
 
-    def calc_mmse(self, channel):
+def generate_batch_channel(num_antennas, num_users, fullspace_dim, batch_size, cur_subspace, base_vectors):
+    coordinates = th.randn(batch_size, cur_subspace, 1)
+    vec_channel = th.bmm(base_vectors[:cur_subspace].T.repeat(batch_size, 1).reshape(batch_size, base_vectors.shape[1], fullspace_dim), coordinates).reshape(-1 ,2 * num_users * num_antennas) * (( 32 / cur_subspace) ** 0.5)
+    return  (num_antennas * num_users) ** 0.5 * (vec_channel / vec_channel.norm(dim=1, keepdim = True))
 
-        K = self.K
-        N = self.N
-        lambda_ = th.ones(self.K).repeat((channel.shape[0], 1)).to(self.device) * self.total_power / self.K
-        p = th.ones(self.K).repeat((channel.shape[0], 1)).to(self.device) * np.sqrt(self.total_power / self.K)
-        effective_channel = channel.conj().transpose(1,2).to(th.cfloat).to(self.device)
-        channel = channel.to(self.device)
-        eye_N = (th.zeros(lambda_.shape[0], self.N) + 1).to(self.device)
-        eye_N = th.diag_embed(eye_N)
-        lambda_ = th.diag_embed(lambda_)
-        channel = th.bmm(lambda_.to(th.cfloat), channel.type(th.cfloat))
-        denominator = th.inverse(eye_N + th.bmm(effective_channel,channel))
-        wslnr_max = th.zeros((lambda_.shape[0], N, K), dtype=th.cfloat).to(self.device)
-        wslnr_max = th.bmm(denominator, effective_channel)
-        wslnr_max = wslnr_max.transpose(1,2)
-        wslnr_max = wslnr_max / wslnr_max.norm(dim=2, keepdim=True)
-        p = th.diag_embed(p)
-        W = th.bmm(p.to(th.cfloat), wslnr_max)
-        return W
+def get_experiment_path(file_name):
+    file_list = os.listdir()
+    if file_name not in file_list:
+        os.mkdir(file_name)
+    file_list = os.listdir('./{}/'.format(file_name))
+    max_exp_id = 0
+    for exp_id in file_list:
+        if int(exp_id) + 1 > max_exp_id:
+            max_exp_id = int(exp_id) + 1
+    os.mkdir('./{}/{}/'.format(file_name, max_exp_id))
+    return f"./{file_name}/{max_exp_id}/"
+
+if __name__  == "__main__":
     
-    def calc_wsr(self,channel, precoder):
-        HTF = channel
-        HTFGW = th.bmm(HTF.to(th.cfloat), precoder.to(th.cfloat).transpose(-1, -2))
-        S = th.abs(th.diagonal(HTFGW, dim1=-2, dim2=-1))**2
-        I = th.sum(th.abs(HTFGW)**2, dim=-1) - th.abs(th.diagonal(HTFGW, dim1=-2, dim2=-1))**2
-        N = 1
-        SINR = S/(I+N)
-        return th.log2(1+SINR).sum(dim=-1).unsqueeze(-1)
+    num_users = 4
+    num_antennas = 4
+    fullspace_dim = 2 * num_users * num_antennas
+    curriculum_base_vectors, _ = th.linalg.qr(th.rand(fullspace_dim, fullspace_dim, dtype=th.float))
+    mid_dim = 512
+    learning_rate=5e-5
+    file_name = "mimo_beamforming"
+    device=th.device("cuda:0" if th.cuda.is_available() else "cpu")
+    policy_net_mimo = Policy_Net_MIMO(mid_dim).to(device)
+    optimizer = th.optim.Adam(policy_net_mimo.parameters(), lr=learning_rate)
+    save_path = get_experiment_path(file_name)
+    try:
+        train_mimo(policy_net_mimo, optimizer, curriculum_base_vectors=curriculum_base_vectors, num_users=num_users, num_antennas=num_antennas, fullspace_dim=fullspace_dim)
+    except KeyboardInterrupt:
+        th.save(policy_net_mimo.state_dict(), save_path+"0.pth")
+        exit()
+    th.save(policy_net_mimo.state_dict(), save_path+"0.pth")
