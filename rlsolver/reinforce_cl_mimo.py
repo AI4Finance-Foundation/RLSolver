a4f29d6f6e6417d4d70d317283e4218e9e92c913
Xiao-Yang Liu <xl2427@columbia.edu>
2022-08-30 07:53:35 -0400

    Update reinforce_cl_mimo.py

--- rlsolver/reinforce_cl_mimo.py
+++ rlsolver/reinforce_cl_mimo.py
@@ -3,17 +3,16 @@ import torch as th
 from envs.mimo_beamforming.env_net_mimo import Policy_Net_MIMO
 from envs.mimo_beamforming.env_mimo import generate_channel_batch
 
-def train_curriculum_learning( policy_net_mimo, optimizer, save_path, device, K=4, N=4, P=10, noise_power=1, num_training_epochs=40000,
-                num_subspace_update_gap=400, num_save_model_gap=1000):
-    # generate basis vectors of an N x K space, using QR decomposition
+def train_curriculum_learning(policy_net_mimo, optimizer, save_path, device, K=4, N=4, P=10, noise_power=1, num_epochs=40000,
+                num_epochs_per_subspace=400, num_epochs_to_save_model=1000):
+    subspace_dim = 1
+    batch_size = 4096
+    episode_length = 6
+    
+    # using QR decomposition to generate basis vectors of an N x K space, 
     basis_vectors, _ = th.linalg.qr(th.rand(2 * K * N, 2 * K * N, dtype=th.float))
-    subspace_dim=1
-    batch_size=4096
-    episode_length=6
-    for epoch in range(num_training_epochs):
-        if (epoch + 1) % num_subspace_update_gap == 0 and subspace_dim < 2 * K * N:
-            subspace_dim +=1
-        if subspace_dim < 2 * K * N:
+    for epoch in range(num_epoches):
+        if subspace_dim <= 2 * K * N:
             vec_H = generate_channel_batch(N, K, batch_size, subspace_dim, basis_vectors).to(device)
         else:
             vec_H = th.randn(batch_size, 2 * K * N, dtype=th.cfloat).to(device)
@@ -29,9 +28,11 @@ def train_curriculum_learning( policy_net_mimo, optimizer, save_path, device, K=
         optimizer.step()
         print(f" training_loss: {loss.mean().item():.3f} | gpu memory: {th.cuda.memory_allocated():3d}")
         if epoch % num_save_model_gap == 0:
-            th.save(policy_net_mimo.state_dict(), save_path + f"{epoch}.pth")
+            th.save(policy_net_mimo.state_dict(), save_path + f"{epoch}.pth")    
+        if (epoch + 1) % num_epoches_per_subspace == 0 and subspace_dim <= 2 * K * N:
+            subspace_dim +=1
             
-def get_experiment_path(env_name):
+def get_cwd(env_name):
     file_list = os.listdir()
     if env_name not in file_list:
         os.mkdir(env_name)
@@ -46,13 +47,12 @@ def get_experiment_path(env_name):
 if __name__  == "__main__":
     N = 4   # number of antennas
     K = 4   # number of users
-    P = 10 # power constraint
+    P = 10  # power constraint
     noise_power = 1
-    
     learning_rate = 5e-5
     
     env_name = "mimo_beamforming"
-    save_path = get_experiment_path(env_name) # folder to save the trained policy net
+    save_path = get_cwd(env_name) # cwd (current work directory): folder to save the trained policy net
     device=th.device("cuda:0" if th.cuda.is_available() else "cpu")
     policy_net_mimo = Policy_Net_MIMO().to(device)
     optimizer = th.optim.Adam(policy_net_mimo.parameters(), lr=learning_rate)
