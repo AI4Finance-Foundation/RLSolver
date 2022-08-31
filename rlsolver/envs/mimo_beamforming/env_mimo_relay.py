9c61fe64bc0f5e9d706e27a0977bd33fb805eaf1
shixun404 <shixun404@gmail.com>
2022-08-31 06:18:28 -0700

    relay

--- rlsolver/envs/mimo_beamforming/env_mimo_relay.py
+++ rlsolver/envs/mimo_beamforming/env_mimo_relay.py
@@ -1,20 +1,20 @@
 import torch as th
 import numpy as np
 class MIMO_Relay():
-    def __init__(self, K=2, N=2, M=2, P=10, noise_power=1, episode_length=6, num_env=4, device=th.device("cuda:0")):
+    def __init__(self, K=2, N=2, M=2, P=10, noise_power=1, episode_length=6, num_env=4096, device=th.device("cuda:0")):
         self.N = N # #antennas base stations
         self.M = M # #antennas relay
         self.K = K # #users
         self.P = P # power
         self.noise_power = noise_power
-        self.basis_vectors_G, _ = th.linalg.qr(th.rand(2 * self.M * self.N, 2 * self.M * self.N, dtype=th.float))
-        self.basis_vectors_H, _ = th.linalg.qr(th.rand(2 * self.K * self.M, 2 * self.K * self.M, dtype=th.float))
+        self.basis_vectors_G, _ = th.linalg.qr(th.rand(2 * self.M * self.N, 2 * self.M * self.N, dtype=th.float)).to(self.device)
+        self.basis_vectors_H, _ = th.linalg.qr(th.rand(2 * self.K * self.M, 2 * self.K * self.M, dtype=th.float)).to(self.device)
         self.subspace_dim_H = 1
         self.subspace_dim_G = 1
         self.num_env = num_env
         self.device = device
         self.episode_length = episode_length
-        self.mat_F0 = th.diag_embed(th.ones(self.num_env, self.M, dtype=th.cfloat))
+        self.mat_F0 = th.diag_embed(th.ones(self.num_env, self.M, dtype=th.cfloat)).to(self.device)
         
     def reset(self,):
         if self.subspace_dim_H <= 2 * self.M * self.K:
@@ -25,10 +25,10 @@ class MIMO_Relay():
             self.vec_G = self.generate_channel_batch(self.M, self.N, self.num_env, self.subspace_dim_G, self.basis_vectors_G).to(self.device)
         else:
             self.vec_G = th.randn(self.num_env, 2 * self.M * self.N, dtype=th.cfloat).to(self.device)
-        self.mat_H = (self.vec_H[:, :self.K * self.M] + self.vec_H[:, self.K * self.M:] * 1.j).reshape(-1, self.K, self.M)
-        self.mat_G = (self.vec_G[:, :self.M * self.N] + self.vec_G[:, self.M * self.N:] * 1.j).reshape(-1, self.M, self.N)
+        self.mat_H = (self.vec_H[:, :self.K * self.M] + self.vec_H[:, self.K * self.M:] * 1.j).reshape(-1, self.K, self.M).to(self.device)
+        self.mat_G = (self.vec_G[:, :self.M * self.N] + self.vec_G[:, self.M * self.N:] * 1.j).reshape(-1, self.M, self.N).to(self.device)
         self.mat_F = self.mat_F0
-        self.mat_HFG = th.bmm(th.bmm(self.mat_H, self.mat_F), self.mat_G)
+        self.mat_HFG = th.bmm(th.bmm(self.mat_H, self.mat_F), self.mat_G).to(self.device)
         self.mat_W = self.calc_mmse(self.mat_HFG, self.mat_F).to(self.device)
         self.num_steps = 0
         return (self.mat_HFG, self.mat_F)
@@ -46,8 +46,8 @@ class MIMO_Relay():
         return (self.mat_H, self.mat_F)
 
     def generate_channel_batch(self, dim1, dim2, batch_size, subspace_dim, basis_vectors):
-        coordinates = th.randn(batch_size, subspace_dim, 1)
-        basis_vectors_batch = basis_vectors[:subspace_dim].T.repeat(batch_size, 1).reshape(-1, 2 * dim1 * dim2, subspace_dim)
+        coordinates = th.randn(batch_size, subspace_dim, 1).to(self.device)
+        basis_vectors_batch = basis_vectors[:subspace_dim].T.repeat(batch_size, 1).reshape(-1, 2 * dim1 * dim2, subspace_dim).to(self.device)
         vec_channel = th.bmm(basis_vectors_batch, coordinates).reshape(-1 ,2 * dim1 * dim2) * (( 2 * dim1 * dim2 / subspace_dim) ** 0.5)
         return  (dim1 * dim2) ** 0.5 * (vec_channel / vec_channel.norm(dim=1, keepdim = True))
     
