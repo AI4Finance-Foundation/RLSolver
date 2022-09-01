342510fe5c34c590d063af864bec0685a69c0bf0
shixun404 <shixun404@gmail.com>
2022-09-01 16:53:14 -0700

    baseline_mmse

--- rlsolver/envs/mimo_beamforming/env_mimo.py
+++ rlsolver/envs/mimo_beamforming/env_mimo.py
@@ -1,7 +1,6 @@
-from tkinter import W
 import torch as th
 import numpy as np
-
+from baseline_mmse import compute_mmse_beamformer
 
 class MIMOEnv():
     def __init__(self, K=4, N=4, P=10, noise_power=1, episode_length=6, num_env=4, device=th.device("cuda:0")):
@@ -21,14 +20,19 @@ class MIMOEnv():
         else:
             self.vec_H = th.randn(self.num_env, 2 * self.K * self.N, dtype=th.cfloat).to(self.device)
         self.mat_H = (self.vec_H[:, :self.K * self.N] + self.vec_H[:, self.K * self.N:] * 1.j).reshape(-1, self.K, self.N)
-        self.mat_W = self.compute_mmse_beamformer(self.mat_H).to(self.device)
+        self.mat_W = compute_mmse_beamformer(self.mat_H).to(self.device)
         self.num_steps = 0
         self.done = False
         return (self.mat_H, self.mat_W)
 
     def step(self, action):
         self.mat_W = action
-        self.reward = self.calc_sum_rate(self.mat_H, self.mat_W)
+        HW = th.bmm(self.mat_H, self.mat_W.transpose(-1, -2))
+        S = th.abs(th.diagonal(HW, dim1=-2, dim2=-1))**2
+        I = th.sum(th.abs(HW)**2, dim=-1) - th.abs(th.diagonal(HW, dim1=-2, dim2=-1))**2
+        N = 1
+        SINR = S/(I+N)
+        self.reward=  th.log2(1+SINR).sum(dim=-1).unsqueeze(-1)
         self.mat_W = self.mat_W.detach()
         self.num_steps += 1
         self.done = True if self.num_steps >= self.episode_length else False
@@ -39,30 +43,3 @@ class MIMOEnv():
         basis_vectors_batch = basis_vectors[:subspace_dim].T.repeat(batch_size, 1).reshape(-1, 2 * K * N, subspace_dim)
         vec_channel = th.bmm(basis_vectors_batch, coordinates).reshape(-1 ,2 * K * N) * (( 2 * K * N / subspace_dim) ** 0.5)
         return  (N * K) ** 0.5 * (vec_channel / vec_channel.norm(dim=1, keepdim = True))
-    
-    def compute_mmse_beamformer(self, channel):
-        channel = channel.to(self.device)
-        lambda_ = th.ones(self.K).repeat((channel.shape[0], 1)) * self.P / self.K
-        p = th.ones(self.K).repeat((channel.shape[0], 1)).to(self.device) * np.sqrt(self.P / self.K)
-        effective_channel = channel.conj().transpose(1,2).to(th.cfloat).to(self.device)
-        eye_N = (th.zeros(lambda_.shape[0], self.N) + 1).to(self.device)
-        eye_N = th.diag_embed(eye_N)
-        lambda_ = th.diag_embed(lambda_)
-        channel = th.bmm(lambda_.to(th.cfloat), channel.type(th.cfloat))
-        denominator = th.inverse(eye_N + th.bmm(effective_channel,channel))
-        wslnr_max = th.zeros((lambda_.shape[0], self.N, self.K), dtype=th.cfloat).to(self.device)
-        wslnr_max = th.bmm(denominator, effective_channel)
-        wslnr_max = wslnr_max.transpose(1,2)
-        wslnr_max = wslnr_max / wslnr_max.norm(dim=2, keepdim=True)
-        p = th.diag_embed(p)
-        W = th.bmm(p.to(th.cfloat), wslnr_max)
-        return W
-
-    def calc_sum_rate(self,channel, precoder):
-        H = channel
-        HW = th.bmm(H.to(th.cfloat), precoder.to(th.cfloat).transpose(-1, -2))
-        S = th.abs(th.diagonal(HW, dim1=-2, dim2=-1))**2
-        I = th.sum(th.abs(HW)**2, dim=-1) - th.abs(th.diagonal(HW, dim1=-2, dim2=-1))**2
-        N = 1
-        SINR = S/(I+N)
-        return th.log2(1+SINR).sum(dim=-1).unsqueeze(-1)
--- rlsolver/envs/mimo_beamforming/env_mimo_relay.py
+++ rlsolver/envs/mimo_beamforming/env_mimo_relay.py
@@ -1,6 +1,6 @@
 import torch as th
 import numpy as np
-
+from baseline_mmse import compute_mmse_beamformer_relay
 
 class MIMORelayEnv():
     def __init__(self, K=2, N=2, M=2, P=10, noise_power=1, episode_length=6, num_env=4096, device=th.device("cuda:0")):
@@ -38,8 +38,14 @@ class MIMORelayEnv():
     def step(self, action):
         self.mat_F = action.detach()
         self.mat_HTFG = th.bmm(th.bmm(self.mat_H.transpose(-1, -2).conj(), self.mat_F), self.mat_G)
-        self.mat_W = self.compute_mmse_beamformer(self.mat_HTFG, self.mat_F).to(self.device)
-        self.reward = self.calc_sum_rate(self.mat_H, action, self.mat_G, self.mat_W)
+        self.mat_W = compute_mmse_beamformer_relay(self.mat_HTFG, self.mat_F).to(self.device)
+        HTF = th.bmm(self.mat_H.conj().transpose(-1,-2), self.mat_F)
+        HTFGW = th.bmm(th.bmm(HTF.to(th.cfloat), self.mat_G), self.mat_W.to(th.cfloat).transpose(-1, -2))
+        S = th.abs(th.diagonal(HTFGW, dim1=-2, dim2=-1))**2
+        I = th.sum(th.abs(HTFGW)**2, dim=-1) - th.abs(th.diagonal(HTFGW, dim1=-2, dim2=-1))**2
+        N = th.norm(HTF, dim=-1)**2 * 1 + self.noise_power
+        SINR = S/(I+N)
+        self.reward = th.log2(1+SINR).sum(dim=-1).unsqueeze(-1)
         self.num_steps += 1
         self.done = True if self.num_steps >= self.episode_length else False
         return self.get_state(), self.reward, self.done
@@ -52,31 +58,3 @@ class MIMORelayEnv():
         basis_vectors_batch = basis_vectors[:subspace_dim].T.repeat(batch_size, 1).reshape(-1, 2 * dim1 * dim2, subspace_dim).to(self.device)
         vec_channel = th.bmm(basis_vectors_batch, coordinates).reshape(-1 ,2 * dim1 * dim2) * (( 2 * dim1 * dim2 / subspace_dim) ** 0.5)
         return  (dim1 * dim2) ** 0.5 * (vec_channel / vec_channel.norm(dim=1, keepdim = True))
-    
-    def compute_mmse_beamformer(self, channel, mat_F):
-        channel = channel.to(self.device)
-        lambda_ = th.ones(self.K).repeat((channel.shape[0], 1)) * self.P / self.K
-        p = th.ones(self.K).repeat((channel.shape[0], 1)).to(self.device) * np.sqrt(self.P / self.K)
-        effective_channel = channel.conj().transpose(1,2).to(th.cfloat).to(self.device)
-        eye_N = (th.zeros(lambda_.shape[0], self.N) + 1).to(self.device)
-        eye_N = eye_N + mat_F.flatten(start_dim=1).norm(dim=1, keepdim=True)
-        eye_N = th.diag_embed(eye_N).to(self.device)
-        lambda_ = th.diag_embed(lambda_).to(self.device)
-        channel = th.bmm(lambda_.to(th.cfloat), channel.type(th.cfloat))
-        denominator = th.inverse(eye_N + th.bmm(effective_channel,channel))
-        wslnr_max = th.zeros((lambda_.shape[0], self.N, self.K), dtype=th.cfloat).to(self.device)
-        wslnr_max = th.bmm(denominator, effective_channel)
-        wslnr_max = wslnr_max.transpose(1,2)
-        wslnr_max = wslnr_max / wslnr_max.norm(dim=2, keepdim=True)
-        p = th.diag_embed(p)
-        W = th.bmm(p.to(th.cfloat), wslnr_max)
-        return W
-
-    def calc_sum_rate(self, H, F, G, W):
-        HF = th.bmm(H.conj().transpose(-1,-2), F)
-        HTFGW = th.bmm(th.bmm(HF.to(th.cfloat), G), W.to(th.cfloat).transpose(-1, -2))
-        S = th.abs(th.diagonal(HTFGW, dim1=-2, dim2=-1))**2
-        I = th.sum(th.abs(HTFGW)**2, dim=-1) - th.abs(th.diagonal(HTFGW, dim1=-2, dim2=-1))**2
-        N = th.norm(HF, dim=-1)**2 * 1 + self.noise_power
-        SINR = S/(I+N)
-        return th.log2(1+SINR).sum(dim=-1).unsqueeze(-1)
