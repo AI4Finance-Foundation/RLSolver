d31366ed887cfa931fce23dc0dd06573377b90f5
Shixun Wu <78099975+shixun404@users.noreply.github.com>
2022-10-04 11:29:32 -0700

    Update baseline_mmse.py

--- rlsolver/envs/mimo_beamforming/baseline_mmse.py
+++ rlsolver/envs/mimo_beamforming/baseline_mmse.py
@@ -4,10 +4,10 @@ import numpy as np
 
 def compute_mmse_beamformer(mat_H, K=4, N=4, P=10, noise_power=1, device=th.device("cpu")):
     eye_N = th.diag_embed((th.zeros(mat_H.shape[0], N) + noise_power)).to(device)
-    denominator = th.inverse(eye_N + th.bmm(mat_H.conj().transpose(1,2), P / K * mat_H))
+    denominator = th.inverse(eye_N + th.bmm(mat_H.conj().transpose(1,2), th.bmm(P / K, mat_H)))
     wslnr_max = th.bmm(denominator, mat_H.conj().transpose(1,2)).transpose(1,2)
     wslnr_max = wslnr_max / wslnr_max.norm(dim=2, keepdim=True)
-    mat_W = wslnr_max * np.sqrt(P/ K)
+    mat_W = th.bmm(wslnr_max, th.sqrt(P/ K))
     mat_HW = th.bmm(mat_H, mat_W.transpose(-1, -2))
     S = th.abs(th.diagonal(mat_HW, dim1=-2, dim2=-1))**2
     I = th.sum(th.abs(mat_HW)**2, dim=-1) - th.abs(th.diagonal(mat_HW, dim1=-2, dim2=-1))**2
