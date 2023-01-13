35206a2f9c0d63db803ece50792c516f6c58c886
shixun404 <shixun404@gmail.com>
2023-01-13 20:00:48 +0800

    update train_dcs

--- rlsolver/rlsolver_compressive_sensing/train_dcs.py
+++ rlsolver/rlsolver_compressive_sensing/train_dcs.py
@@ -3,16 +3,17 @@ import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
 from file_utils import *
+from copy import deepcopy
 
 class Generator(nn.Module):
-    def __init__(self, in_dim=500, out_dim=28**2, mid_dim = 512):
+    def __init__(self, in_dim=2, out_dim=28**2, mid_dim = 512):
         super(Generator, self).__init__()
         self.in_dim = in_dim
         self.out_dim = out_dim
         self.mid_dim = mid_dim
         self.net = nn.Sequential(
-            nn.Linear(self.in_dim, mid_dim),nn.ReLU(),
-            nn.Linear(self.mid_dim, self.mid_dim),nn.ReLU(),
+            nn.Linear(self.in_dim, mid_dim),nn.LeakyReLU(),
+            nn.Linear(self.mid_dim, self.mid_dim),nn.LeakyReLU(),
             nn.Linear(self.mid_dim, self.out_dim),nn.Tanh()
         )
 
@@ -36,8 +37,8 @@ class Metric(nn.Module):
         )
 
     def forward(self, input):
-        return torch.bmm(self.measure, input.unsqueeze(dim=-1))
-        #return self.net(input)
+        #return torch.bmm(self.measure, input.unsqueeze(dim=-1))
+        return self.net(input)
 
 
 def load_data(batch_size):
@@ -53,52 +54,69 @@ def load_data(batch_size):
     return train_loader
 
 
-def train_dcs(latent_dim=500, batch_size=64, num_training_epoch=100, lr=5e-4, initial_step_size=0.01, num_grad_iters=3, device=torch.device("cuda:0")):
+def train_dcs(latent_dim=2, batch_size=64, num_training_epoch=100, lr=1e-4, initial_step_size=0.01, num_grad_iters=4, device=torch.device("cuda:0")):
     file_exporter = FileExporter('./image', )
     training_data = load_data(batch_size)
 
     gen = Generator().to(device)
     measurement = Metric().to(device)
     optimizer = torch.optim.Adam(gen.parameters(), lr=lr)
+    optimizer_m = torch.optim.Adam(measurement.parameters(), lr=lr)
     MSELoss = nn.MSELoss()
-    step_size = nn.Parameter(torch.as_tensor([initial_step_size])).to(device)
+    step_size = torch.ones(1).to(device) * np.log(initial_step_size)
+    print(step_size)
+    optimizer_s = torch.optim.Adam([step_size], lr=lr)
+    step_size.requires_grad_()
     for epoch in range(num_training_epoch):
         for i, (images, labels) in enumerate(training_data):
             if (images.shape[0] == 32):
                 continue
             original_data = images.reshape(batch_size, -1).to(device)
+            #$assert 0
             original_data = (original_data) * 2 - 1
-            z_initial = torch.randn(batch_size, latent_dim, device=device, requires_grad=True)
-            measurement_original_data = measurement(original_data)
+            z_initial = torch.randn(batch_size, latent_dim, device=device, requires_grad=False)
+            z_initial = z_initial / z_initial.norm(keepdim=True, dim=-1).detach()
+            measurement_original_data = measurement(original_data).detach()
             z = [z_initial for _ in range(num_grad_iters)]
+            z_ = [z_initial for _ in range(num_grad_iters)]
+            z__ = []
+            s = deepcopy(step_size.exp().detach())
             for itr in range(1, num_grad_iters):
-                z[itr - 1].requires_grad_()
-                t = measurement(gen(z[itr - 1]))
+                z_[itr-1].requires_grad_()
+                t = measurement(gen(z_[itr-1]))
 
                 MSELoss(t, measurement_original_data).backward()
-                z[itr] = z[itr - 1] - step_size * z[itr - 1].grad
-                z[itr] = (z[itr] / z[itr].norm(keepdim=True)).detach()
-            z_optimized = z[-1]
-            generated_data_initial = gen(z_optimized)
+                z__.append(z_[itr-1].grad.detach())
+                z[itr] = z[itr - 1] - step_size.exp() * z_[itr-1].grad
+                z[itr] = (z[itr] / z[itr].norm(keepdim=True, dim=-1))
+                z_[itr] = z_[itr-1] - s * z_[itr-1].grad
+                z_[itr] = (z_[itr] / z_[itr].norm(keepdim=True, dim=-1)).detach()
+            z_optimized = z_initial - step_size.exp() * (z__[0] + z__[1] + z__[2])
+            generated_data_initial = gen(z_initial)
             generated_data_optimized = gen(z_optimized)
-
+            measurement_original_data.requires_grad_()
             measurement_generated_data_initial = measurement(generated_data_initial)
             measurement_generated_data_optimized = measurement(generated_data_optimized)
-            generated_loss = MSELoss(measurement_generated_data_optimized, measurement_original_data)
-            RIP_loss = MSELoss((measurement_generated_data_initial - measurement_original_data).norm(), \
-                                (generated_data_initial - original_data).norm())
-            RIP_loss += MSELoss((measurement_generated_data_optimized - measurement_original_data).norm(), \
-                                (generated_data_optimized - original_data).norm())
-            RIP_loss += MSELoss((measurement_generated_data_optimized - measurement_generated_data_initial).norm(), \
-                                (generated_data_optimized - generated_data_initial).norm())
+            generated_loss = MSELoss(measurement_generated_data_optimized, measurement_original_data)#.norm(dim=-1).square().mean()
+            RIP_loss = MSELoss((measurement_generated_data_initial - measurement_original_data).reshape(-1, 25).norm(dim=-1), \
+                                (generated_data_initial - original_data).norm(dim=-1))#.square().mean()
+            RIP_loss += MSELoss((measurement_generated_data_optimized - measurement_original_data).reshape(-1, 25).norm(dim=-1), \
+                                (generated_data_optimized - original_data).norm(dim=-1))#.square().mean()
+            RIP_loss += MSELoss((measurement_generated_data_optimized - measurement_generated_data_initial).reshape(-1, 25).norm(dim=-1), \
+                                (generated_data_optimized - generated_data_initial).norm(dim=-1))#.square().mean()
             loss = generated_loss + RIP_loss / 3
             if  i % 50 ==  0:
                 print(loss.item())
+                #print((generated_data_optimized[0] - generated_data_optimized[1] ) / 2 * 255)
                 file_exporter.save((original_data.detach().reshape(batch_size, 28, 28, 1).cpu().numpy() + 1) / 2, 'origin')
                 file_exporter.save((generated_data_optimized.detach().reshape(batch_size, 28, 28, 1).cpu().numpy() + 1) / 2, 'reconstruction')
             optimizer.zero_grad()
+            optimizer_s.zero_grad()
+            optimizer_m.zero_grad()
             loss.backward()
             optimizer.step()
+            optimizer_s.step()
+            optimizer_m.step()
 
 if __name__ == "__main__":
     train_dcs()
