0fa4547dc89bc068a6d72ace85b8504e88bf3eb2
SpicyWei <75991833+spicywei@users.noreply.github.com>
2023-02-16 17:38:26 +0800

    Update brute_force.py

--- rlsolver/rlsolver_quantum_circuits/Tensor_Ring/brute_force.py
+++ rlsolver/rlsolver_quantum_circuits/Tensor_Ring/brute_force.py
@@ -3,49 +3,66 @@ import torch as th
 from copy import deepcopy
 import numpy as np
 
-
 device = th.device("cuda:0")
 reward = 0
 # 根据张量网络中张量数量设定
-N = 100
-
-with open(f"test_data_tensor_train_N={N}.pkl", 'rb') as f:
+N = 4
+with open(f"test_data_tensor_ring_N={N}.pkl", 'rb') as f:
     a = pkl.load(f).detach().cpu()
 num_env = a.shape[0]
 # 每个env中包含的sample数量
-num_samples = 5000
+num_samples = 500
 start_ = th.as_tensor([i for i in range(N)]).repeat(1, num_env).reshape(num_env, -1).to(device)
 end_ = th.as_tensor([i for i in range(N)]).repeat(1, num_env).reshape(num_env, -1).to(device)
 reward = th.zeros(num_env, num_samples)
-permute_record = th.zeros(num_env, num_samples, N - 1)
+permute_record = th.zeros(num_env, num_samples, N)
 min_best = 4e+31
 np.set_printoptions(suppress=True)
 for k in range(num_env):
     best_reward = 4e+31
     for permute_i in range(num_samples):
-        permute = th.randperm(N - 1)
-        r = 0
-        state = a[k]
+        permute = th.randperm(N)
+        rtp = 0
+        state = deepcopy(a[k])
         start = deepcopy(start_[k]) + 1
         end = deepcopy(end_[k]) + 1
-        for i in permute:
-            tmp = 1
-            for j in range(start[i], end[i] + 1):
-                tmp *= (state[j, j] * state[j, start[i] - 1] * state[end[i] + 1, j])
-            for j in range(start[i + 1], end[i + 1] + 1):
-                tmp *= (state[j, j] * state[j, start[i + 1] - 1] * state[end[i + 1] + 1, j])
-            tmp = tmp / state[start[i + 1], start[i + 1] - 1]
-            start_new = min(start[i], start[i + 1])
-            end_new = max(end[i], end[i + 1])
-            for __ in range(start_new, end_new + 1):
-                start[__ - 1] = start_new
-                end[__ - 1] = end_new
+        for i in (permute[:-2]):
+            r = 1
+            print(state)
+            if (i == N-1):
+                first_node = (i + 1) % N
+                second_node = (i + 2) % N
+            else:
+                first_node = (i + 2) % N
+                second_node = (i + 1) % N
+            if (first_node == 0):    first_node = N
+            if (second_node == 0):    second_node = N
 
-            r += tmp
-        reward[k, permute_i] = r
+            if start[i] == 1 or end[i] == N:
+                r = r * state[N, 1]
+            if start[(i + 1) % N] == 1 or end[(i + 1) % N] == N:
+                r = r * state[N, 1]
+            tmp1 = start[i]
+            tmp2 = start[(i + 1) % N]
+            for i in range(N):
+                if ((start[i] == tmp1) or (start[i] == tmp2)):
+                    r = r * (state[i + 1, i + 1] * state[i + 1, i] * state[i + 2, i + 1])
+            r /= 2
+            state[first_node, second_node] = 1
+            s1 = 0 + start[i]
+            s2 = 0 + start[(i + 1) % N]
+            start_new = min(start[i], start[(i + 1) % N])
+            end_new = max(end[i], end[(i + 1) % N])
+            for i in range(N):
+                if ((start[i] == s1) or (start[i] == s2)):
+                    start[i] = start_new
+                    end[i] = end_new
+
+            rtp += r
+        reward[k, permute_i] = rtp
         # print(permute, permute_i)
         permute_record[k, permute_i] = permute
-        best_reward = min(best_reward, r)
+        best_reward = min(best_reward, rtp - 2 ** (N + 1))
     min_best = min(best_reward, min_best)
 
     # print(reward[k], permute_record[k, reward[k].min(dim=-1)[1]], best_reward.numpy(), min_best.numpy())
@@ -55,11 +72,11 @@ for k in range(num_env):
     print(best_reward_str, min_best_str)
 # 所有env中最优reward的平均值
 print(reward.min(dim=-1)[0].mean().numpy())
-with open("record_r_baseline_random.pkl", "wb") as f:
-    import pickle as pkl
-
-    pkl.dump(reward, f)
-with open("record_permute_baseline_random.pkl", "wb") as f:
-    import pickle as pkl
-
-    pkl.dump(permute_record, f)
+# with open("record_r_baseline_random.pkl", "wb") as f:
+#     import pickle as pkl
+#
+#     pkl.dump(reward, f)
+# with open("record_permute_baseline_random.pkl", "wb") as f:
+#     import pickle as pkl
+#
+#     pkl.dump(permute_record, f)
