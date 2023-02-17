9c787f074ca6eebe5c19d9cf08aecff0eba26fb8
SpicyWei <75991833+spicywei@users.noreply.github.com>
2023-02-17 11:28:59 +0800

    Update and rename brute_force.py to Ring_brute_force.py

--- rlsolver/rlsolver_quantum_circuits/Tensor_Ring/Ring_brute_force.py
+++ rlsolver/rlsolver_quantum_circuits/Tensor_Ring/Ring_brute_force.py
@@ -5,13 +5,11 @@ import numpy as np
 
 device = th.device("cuda:0")
 reward = 0
-# 根据张量网络中张量数量设定
-N = 6
+N = 100
 with open(f"test_data_tensor_ring_N={N}.pkl", 'rb') as f:
     a = pkl.load(f).detach().cpu()
 num_env = a.shape[0]
-# 每个env中包含的sample数量
-num_samples = 500
+num_samples = 5000
 start_ = th.as_tensor([i for i in range(N)]).repeat(1, num_env).reshape(num_env, -1).to(device)
 end_ = th.as_tensor([i for i in range(N)]).repeat(1, num_env).reshape(num_env, -1).to(device)
 reward = th.zeros(num_env, num_samples)
@@ -65,19 +63,15 @@ for k in range(num_env):
         permute_record[k, permute_i] = permute
         best_reward = min(best_reward, rtp)
     min_best = min(best_reward, min_best)
-
-    # print(reward[k], permute_record[k, reward[k].min(dim=-1)[1]], best_reward.numpy(), min_best.numpy())
     best_reward_str = str(best_reward.numpy())
     min_best_str = str(min_best.numpy())
-    # 分别输出  当前env中最优的reward、所有env中最优的reward
     print(best_reward_str, min_best_str)
-# 所有env中最优reward的平均值
 print(reward.min(dim=-1)[0].mean().numpy())
-# with open("record_r_baseline_random.pkl", "wb") as f:
-#     import pickle as pkl
-#
-#     pkl.dump(reward, f)
-# with open("record_permute_baseline_random.pkl", "wb") as f:
-#     import pickle as pkl
-#
-#     pkl.dump(permute_record, f)
+with open("record_r_baseline_random.pkl", "wb") as f:
+    import pickle as pkl
+
+    pkl.dump(reward, f)
+with open("record_permute_baseline_random.pkl", "wb") as f:
+    import pickle as pkl
+
+    pkl.dump(permute_record, f)
