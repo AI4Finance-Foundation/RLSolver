816d7a92f63222bc2c342ec3a7ce5ee006d7ea5e
SpicyWei <75991833+spicywei@users.noreply.github.com>
2023-02-17 11:30:55 +0800

    Update brute_force.py
    The latest version of Train_brute_force

--- rlsolver/rlsolver_quantum_circuits/Tensor_Train/brute_force.py
+++ rlsolver/rlsolver_quantum_circuits/Tensor_Train/brute_force.py
@@ -3,16 +3,12 @@ import torch as th
 from copy import deepcopy
 import numpy as np
 
-
 device = th.device("cuda:0")
 reward = 0
-# 根据张量网络中张量数量设定
 N = 100
-
 with open(f"test_data_tensor_train_N={N}.pkl", 'rb') as f:
     a = pkl.load(f).detach().cpu()
 num_env = a.shape[0]
-# 每个env中包含的sample数量
 num_samples = 5000
 start_ = th.as_tensor([i for i in range(N)]).repeat(1, num_env).reshape(num_env, -1).to(device)
 end_ = th.as_tensor([i for i in range(N)]).repeat(1, num_env).reshape(num_env, -1).to(device)
@@ -47,19 +43,13 @@ for k in range(num_env):
         permute_record[k, permute_i] = permute
         best_reward = min(best_reward, r)
     min_best = min(best_reward, min_best)
-
-    # print(reward[k], permute_record[k, reward[k].min(dim=-1)[1]], best_reward.numpy(), min_best.numpy())
     best_reward_str = str(best_reward.numpy())
     min_best_str = str(min_best.numpy())
-    # 分别输出  当前env中最优的reward、所有env中最优的reward
     print(best_reward_str, min_best_str)
-# 所有env中最优reward的平均值
 print(reward.min(dim=-1)[0].mean().numpy())
 with open("record_r_baseline_random.pkl", "wb") as f:
     import pickle as pkl
-
     pkl.dump(reward, f)
 with open("record_permute_baseline_random.pkl", "wb") as f:
     import pickle as pkl
-
     pkl.dump(permute_record, f)
