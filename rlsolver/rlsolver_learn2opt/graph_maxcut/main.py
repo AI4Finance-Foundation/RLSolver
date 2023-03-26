207eb760dcc48025430cd36013a4e02b68c565b7
Ming Zhu <zhumingpassional@gmail.com>
2023-03-26 15:20:50 +0800

    remove duplicates

--- rlsolver/rlsolver_learn2opt/graph_maxcut/main.py
+++ rlsolver/rlsolver_learn2opt/graph_maxcut/main.py
@@ -120,7 +120,6 @@ if __name__ == '__main__':
     sparsity= float(sys.argv[2])
     gpu_id = int(sys.argv[3])
     device = th.device(f"cuda:{gpu_id}" if (th.cuda.is_available() and (gpu_id >= 0)) else "cpu")
-    look_ahead_K = 5
     look_ahead_K = 20
     obj_fun = Obj_fun
     opt_variable_class = Opt_variable
