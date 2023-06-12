7655cc947f8b25fa605a103f9094f4dbefbed249
mingzhu <zhumingpassional@gmail.com>
2023-06-12 15:37:10 +0800

    revise_reset

--- rlsolver/envs/_base_env.py
+++ rlsolver/envs/_base_env.py
@@ -18,12 +18,12 @@ class _BaseEnv():
     def load_graph(self, file_name: str):
         self.adjacency_matrix = th.as_tensor(np.load(file_name), device=self.device)
 
-    def reset(self, add_noise_for_best_x=False, sample_ratio_envs=0.2, sample_ratio_nodes=0.2):
-        if add_noise_for_best_x and self.best_x is not None:
+    def reset(self, best_x=False, sample_ratio_envs=0.6, sample_ratio_nodes=0.7):
+        if best_x and self.best_x is not None:
             e = max(1, int(sample_ratio_envs * self.num_envs))
             n = max(1, int(sample_ratio_nodes * self.num_nodes))
-            indices_envs = th.randint(0, self.num_envs, e)  # indices of selected envs/rows
-            indices_nodes = th.randint(0, self.num_nodes, n)
+            indices_envs = th.randint(0, self.num_envs, (e, 1))  # indices of selected envs/rows
+            indices_nodes = th.randint(0, self.num_nodes, (n, 1))
             # noise = th.randn(n, self.num_nodes).to(self.device)
             noise = th.rand(self.num_envs, self.num_nodes).to(self.device)
             mask = th.zeros(self.num_envs, self.num_nodes, dtype=bool).to(self.device)
@@ -32,13 +32,13 @@ class _BaseEnv():
 
             mask2 = th.zeros(self.num_envs, self.num_nodes, dtype=bool).to(self.device)
             mask2[indices_envs, :] = True
-            add_noise_for_best_x = th.mul(self.best_x.repeat(self.num_envs, 1), mask2).to(self.device)
+            best_x = th.mul(self.best_x.repeat(self.num_envs, 1), mask2).to(self.device)
 
-            mask3 = th.ones(self.num_envs, self.num_nodes, dtype=bool).to(self.device)
+            mask3 = th.ones(self.num_envs, self.num_nodes, dtype=bool)
             mask3[indices_envs, :] = False
             x = th.mul(th.rand(self.num_envs, self.num_nodes), mask3).to(self.device)
 
-            self.x = x + add_noise_for_best_x + noise
+            self.x = x + best_x + noise
             self.x[0, :] = self.best_x  # the first row is best_x, no noise
         else:
             self.x = th.rand(self.num_envs, self.num_nodes).to(self.device)
