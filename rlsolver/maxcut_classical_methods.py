1844fae014a4d69c89a44e4fe8ac8c2bf53c53fc
mingzhu <zhumingpassional@gmail.com>
2023-07-07 16:15:47 +0800

    revise greedy

--- rlsolver/maxcut_classical_methods.py
+++ rlsolver/maxcut_classical_methods.py
@@ -27,32 +27,30 @@ def random_walk(init_solution: Tensor, num_steps: int, env: GraphMaxCutEnv) -> (
 
 
 
-def greedy(init_solution: Tensor, env: GraphMaxCutEnv) -> (int, Tensor):
-    curr_node = 0
-    visited = [0] * env.num_nodes
-    visited[0] = 1
+def greedy(start_node: int, env: GraphMaxCutEnv) -> (int, Tensor):
+    nodes = list(range(env.num_nodes))
+    visited = [start_node]
     curr_solution: Tensor = th.zeros((1, env.num_nodes))
     curr_score: int = int(-env.get_objective(curr_solution)[0])
-    for i in range(env.num_nodes - 1):
+    for iteration in range(env.num_nodes - 1):
+        print("iteration in greedy: ", iteration)
         tmp_solutions = []
         tmp_scores = []
         tmp_nodes = []
         nodes_to_visit = set()
         for k in range(len(visited)):
-            if visited[k] == 1:
-                will_visit = env.get_neighbor_nodes(k)
-                to_visit = [item for item in will_visit if visited[item] == 0]
-                nodes_to_visit = nodes_to_visit | set(to_visit)
+            will_visit = env.get_neighbor_nodes(k)
+            to_visit = [item for item in will_visit if item not in visited]
+            nodes_to_visit = nodes_to_visit | set(to_visit)
         nodes_to_visit = list(nodes_to_visit)
         for j in range(len(nodes_to_visit)):
             node = int(nodes_to_visit[j])
-            if visited[node] == 0:
-                tmp_solution = copy.deepcopy(curr_solution)
-                tmp_solution[0, node] = (tmp_solution[0, node] + 1) % 2
-                tmp_score = int(-env.get_objective(tmp_solution)[0])
-                tmp_scores.append(tmp_score)
-                tmp_solutions.append(tmp_solution)
-                tmp_nodes.append(node)
+            tmp_solution = copy.deepcopy(curr_solution)
+            tmp_solution[0, node] = (tmp_solution[0, node] + 1) % 2
+            tmp_score = int(-env.get_objective(tmp_solution)[0])
+            tmp_scores.append(tmp_score)
+            tmp_solutions.append(tmp_solution)
+            tmp_nodes.append(node)
         if len(tmp_scores) >= 1:
             best_tmp_score = max(tmp_scores)
             index = tmp_scores.index(best_tmp_score)
@@ -61,8 +59,7 @@ def greedy(init_solution: Tensor, env: GraphMaxCutEnv) -> (int, Tensor):
             if best_tmp_score > curr_score:
                 curr_score = best_tmp_score
                 curr_solution = best_tmp_solution
-                curr_node = best_tmp_node
-                visited[curr_node] = 1
+                visited.append(best_tmp_node)
         else:
             return curr_score, curr_solution
 
@@ -72,9 +69,10 @@ def greedy(init_solution: Tensor, env: GraphMaxCutEnv) -> (int, Tensor):
 if __name__ == '__main__':
     env = GraphMaxCutEnv(graph_key='gset_70')
     init_solution = th.randint(0, 1+1, (1, env.num_nodes))
-    rw_score, rw_solution = random_walk(init_solution=init_solution, num_steps=10000, env=env)
+    # rw_score, rw_solution = random_walk(init_solution=init_solution, num_steps=10000, env=env)
 
-    gr_score, gr_solution = greedy(init_solution=init_solution, env=env)
+    start_node = random.randint(0, env.num_nodes - 1)
+    gr_score, gr_solution = greedy(start_node=start_node, env=env)
 
 
 
