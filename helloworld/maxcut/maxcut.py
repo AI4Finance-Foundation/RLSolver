3c5e76dde13381b08cbcd3f99cb06b051c939292
mingzhu <zhumingpassional@gmail.com>
2023-07-11 10:40:12 +0800

    revise path

--- helloworld/maxcut/maxcut.py
+++ helloworld/maxcut/maxcut.py
@@ -23,7 +23,7 @@ def train(num_nodes: int,
           hidden_layer_size: int):
     maxcut_env = MaxcutEnv(num_nodes=num_nodes, num_envs=num_envs, device=device, episode_length=episode_length)
 
-    maxcut_env.load_graph(f"./data/maxcut/gset_{sys.argv[1]}.npy")
+    maxcut_env.load_graph(f"./data/gset_{sys.argv[1]}.npy")
     l_num = 1
     h_init = th.zeros(l_num, num_envs, hidden_layer_size).to(device)
     c_init = th.zeros(l_num, num_envs, hidden_layer_size).to(device)
