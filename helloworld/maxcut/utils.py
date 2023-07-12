babc3a9fb2059e88301c2c2a161f0a7e9d9e5912
mingzhu <zhumingpassional@gmail.com>
2023-07-12 22:50:18 +0800

    add weight_low, weight_high for generate_symmetric_adjacency_matrix

--- helloworld/maxcut/utils.py
+++ helloworld/maxcut/utils.py
@@ -50,11 +50,12 @@ def write_result(result: Tensor, filename: str='result/result.txt'):
         for i in range(N):
             file.write(f'{i} {int(result[i])}\n')
 
-def generate_symmetric_adjacency_matrix(num_nodes:int, density: float): # sparsity for binary
+def generate_symmetric_adjacency_matrix(num_nodes:int, density: float, weight_low=0, weight_high=2): # sparsity for binary
     # upper_triangle = th.mul(th.rand(num_nodes, num_nodes).triu(diagonal=1),
     #                         (th.rand(num_nodes, num_nodes) < sparsity).int().triu(diagonal=1))
     upper_triangle = torch.triu((th.rand(num_nodes, num_nodes) < density).int(), diagonal=1)
-    adjacency_matrix = upper_triangle + upper_triangle.transpose(-1, -2)
+    upper_triangle2 = th.mul(th.randint(weight_low, weight_high, (num_nodes, num_nodes)), upper_triangle)
+    adjacency_matrix = upper_triangle2 + upper_triangle2.transpose(-1, -2)
     return adjacency_matrix
     
 def write_symmetric_adjacency_matrix(adjacency_matrix: Tensor, filename: str='data/graph.txt'):
